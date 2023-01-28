66f13da465eecb263c07ac150796a2249f5eab7b
kaif22622 <kaixin.feng@yahoo.com>
2023-01-28 14:42:17 -0600

    added input feature

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,16 +2,22 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val input = findViewById<EditText>(R.id.editTextSomething).text
+
         val btn = findViewById<Button>(R.id.toast_button)
         btn.setOnClickListener{
-            Toast.makeText(applicationContext, "Hello~", Toast.LENGTH_SHORT).show()
+            Toast.makeText(applicationContext, input, Toast.LENGTH_SHORT).show()
         }
     }
 }
